36af6fe66f92941cfbe2f13e42f4fc6d61c8373f
Yulya <secondnumber@Yulyas-MacBook-Air.local>
2022-03-13 17:12:41 +0300

    init: Initial commit

---
+++ client/dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+const sayHello = () => console.log('hello');
+sayHello();
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
